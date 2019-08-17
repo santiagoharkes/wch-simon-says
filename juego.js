89c9ba36fd4d57bf3ea6a22c73c2f62ed5d8fb3d
Santiago <santiagoharkes@gmail.com>
2019-08-17 09:46:28 -0300

    Agregando una rama nueva para modificaciones y funcion para iluminar colores progresivamente

--- juego.js
+++ juego.js
@@ -11,12 +11,13 @@ class Juego {
     constructor() { // En este caso no recibe ningun parámetro pero si métodos/acciones
         this.inicializar() // Podemos declarar cualquier método/accion de THIS (o sea de JUEGO) aunque no exista, y después escribirla
         this.generarSecuencia()
+        this.siguienteNivel()
     }
 
     inicializar() {
         btnEmpezar.classList.add('hide')
         this.nivel = 1
-        this.colores = {
+        this.colores = { // Acá ponemos a los BOTONES con el metodo COLORES para tenerlos más fácil
             // celeste: celeste
             // Si queremos en un objeto poner Atributo "celeste" y asignarle el valor que tiene en la variable celeste (la de arriba),
             // Podemos escribirla así
@@ -37,6 +38,48 @@ class Juego {
         // Math.floor va a redondear el numero para abajo
         this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
+
+    siguienteNivel() {
+        this.iluminarSecuencia()
+    }
+
+        transformarNumeroAColor(numero){
+            switch (numero) {
+                case 0:
+                    return 'celeste'
+                case 1:
+                    return 'violeta'
+                case 2:
+                    return 'naranja'
+                case 3:
+                    return 'verde'
+            }
+        }
+    
+        // Esto va a recorrer el array secuencia hasta el nivel en el que esté el usuario
+        // El array secuencia se crea solamente UNA VEZ
+        // Y es el mismo para todo el juego
+        iluminarSecuencia() {
+            for (let i = 0; i < this.nivel; i++) {
+                const color = this.transformarNumeroAColor(this.secuencia[i])
+                // La función de arriba hace lo siguiente
+                // El parametro this.secuencia[i] hace referencia a la posicion en la secuencia (array)
+                // Si la posición es 1 (primer nivel) y el array es 0 2 2 3 1
+                // El numero que devuelve va a ser 0
+                // Esto se lo pasa a transformarNumeroAColor, y esta funcion agarra el 0 y lo pasa a 'celeste'
+                // Este 'celeste' se guarda en la variable color
+                setTimeout(() => this.iluminarColor(color), 1000 * i )
+            }
+        }
+
+        iluminarColor(color) {
+            this.colores[color].classList.add('light')
+            setTimeout(() => this.apagarColor(color), 350)
+        }
+
+        apagarColor(color) {
+            this.colores[color].classList.remove('light')
+        }
 }
 
 function empezarJuego() { // Esta función de empezar juego, esta declarada en el BOTON de HTML
